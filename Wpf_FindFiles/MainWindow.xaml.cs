e180cd05fae2426a6ba7d37bccd44559ebd17343
Gao Shengxu <1059067158@qq.com>
2016-02-29 21:39:17 +0800

    2016/2/29 21:37
    Now,I finish the principal interface of the clone software. I think it
    is a good beginning.

---
+++ Wpf_FindFiles/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_FindFiles
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFileSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnBeginSearch(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBeginSearch_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ Wpf_FindFiles/NumberConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Wpf_FindFiles
+{   
+    class NumberConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            long longValue = (long)value;
+
+            double temp = 0;
+            if ((longValue >= 1024) && (longValue < 1024 * 1024))
+            {
+                temp = (double)longValue / 1024;
+                return temp.ToString("N") + "K";
+            }
+
+            if ((longValue >=1024*1024)&&(longValue<1024*1024*1024))
+            {
+                temp=(double)longValue/(1024*1024);
+                return temp.ToString("N") + "M";
+            }
+
+            if((longValue>=1024*1024*1024))
+            {
+                temp=(double)longValue/(1024*1024*1024);
+                return temp.ToString("N") + "G";
+            }
+
+            return longValue.ToString() + "字节";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
