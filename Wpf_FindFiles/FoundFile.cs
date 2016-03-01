f186a69e3069f6fb78fe34afbd27972febdafd5c
Gao Shengxu <1059067158@qq.com>
2016-03-01 16:58:39 +0800

    2016/3/1 processing.
    I code maybe 30%-50% of All.However, there are still some bugs.Tonight,i
    have a class of analysis and design of algorithms.So Continue on
    tomorrow.

---
+++ Wpf_FindFiles/FoundFile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_FindFiles
+{
+    public class FoundFile :INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    OnPropertyChanged("Name");
+                    _name = value;
+                }
+            }
+        }
+        private long _length;
+        public long Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if(_length!=value)
+                {
+                    OnPropertyChanged("Length");
+                    _length=value;
+                }
+            }
+        }
+
+        private string _location;
+        public string Location
+        {
+            get
+            {
+                return _location;            
+            }
+            set
+            {
+                if (_location != value)
+                {
+                    OnPropertyChanged("Location");
+                    _location = value;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- Wpf_FindFiles/MainWindow.xaml.cs
+++ Wpf_FindFiles/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +25,12 @@ namespace Wpf_FindFiles
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<FoundFile> FoundFiles = null;
+
+        private CancellationTokenSource cts = null;
+
+        Action<String> showInfo = null;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,14 +41,82 @@ namespace Wpf_FindFiles
 
         }
 
-        private void btnBeginSearch(object sender, RoutedEventArgs e)
+        private void searchFiles(String Location, long fileLength)
         {
+            DirectoryInfo dir = null;
+            List<FileInfo> files = null;
+            FoundFile foundFile=null;
+            try 
+            {
+                dir = new DirectoryInfo(Location);
+                Dispatcher.Invoke(showInfo, Location);
+                if (cts.IsCancellationRequested)
+                {
+                    return;
+                }
+                var query = (from file in dir.GetFiles("*.*", SearchOption.TopDirectoryOnly).AsParallel()
+                             where file.Length >= fileLength
+                             select file).WithCancellation(cts.Token);
 
+                files = query.ToList();
+
+                if (files != null)
+                {
+                    foreach (var item in files)
+                    {
+                        if (cts.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                        foundFile = new FoundFile()
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            Location = item.DirectoryName     //逗号，为什么？
+                        };
+                        Action<FoundFile> addFileDelegte = (file) =>
+                            {
+                                FoundFiles.Add(file);
+                            };
+                        Dispatcher.BeginInvoke(addFileDelegte, foundFile);
+                    }
+                }
+
+                foreach (var directory in dir.GetDirectories())
+                {
+                    searchFiles(directory.FullName, fileLength);
+                }
+            }
+
+             foreach (var directory in dir.GetDirectories())
+            {
+                searchFiles(directory.FullName, fileLength);
+            }
         }
+          catch (UnauthorizedAccessException ex)
+    {
+        Dispatcher.Invoke(showInfo,Directory.Name+"无权限访问");
+    }
+}
+
+     
 
         private void btnBeginSearch_Click(object sender, RoutedEventArgs e)
         {
-           
+            lbllnfo.Text = "正在查找";
+            FoundFiles.Clear();
+            cts = new CancellationTokenSource();
+
+            btnBeginSearch.IsEnabled = false;
+            String Location = txtLocation.Text;
+            long length = Convert.ToInt32(txtFileSize.Text) * 1024 * 1024;
+            Task tsk=new Task(()=>
+                {
+                    searchFiles(Location,length);
+                
+                }
+
+
         }
     }
 }
